0c44ee1798edba7fd33bb05153fdd3c8c1290fce
ranjanmishra <ranjanmishra@convertcart.com>
2018-12-05 11:08:17 +0530

    added Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch } from 'react-router'
 import { Provider } from 'react-redux';
 import store from './store.js';
 import jwt_decode from 'jwt-decode';
